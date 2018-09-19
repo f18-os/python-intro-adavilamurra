cf7726b2ee88eaca35f25a1e42969b830b973ee0
Alejandro Davila <adavilamurra@miners.utep.edu>
2018-09-19 13:53:01 -0600

    Updated code, input redirection working now but piping still not working

--- shell/shell.py
+++ shell/shell.py
@@ -1,29 +1,30 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
+import os, sys, time, re, fileinput
 
-def run_program(args, cmd):
+def run_execve(args):
     for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, cmd)
+        program = "%s/%s" % (dir, args[0].strip())
         try:
             os.execve(program, args, os.environ) # try to exec program
         except FileNotFoundError:             # ...expected
             pass                              # ...fail quietly 
 
-def run_command(user_input, output_file):
+def run_command(user_input):
     pid = os.getpid()               # get and remember parent pid
-    os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
     
-    inputFlag = False
-    outputFlag = False
-    pipeFlag = False
+    inputFlag = outputFlag = pipeFlag = False
 
     # check if there's redirection or piping and get commands
     if ' | ' in user_input:
         processes = user_input.split(' | ')
         args = processes[0]
         pipeFlag = True
-        r, w = os.pipe()      # file descriptors r, w for reading and writing
+        pr, pw = os.pipe()      # file descriptors r, w for reading and writing
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
+        print("pipe fds: pr=%d, pw=%d" % (pr,pw))
+        
     elif ' > ' in user_input:
         args = user_input.split(' > ')
         outputFlag = True
@@ -31,85 +32,92 @@ def run_command(user_input, output_file):
         if ".txt" not in output_file:            # add .txt to files that don't have it
             output_file = output_file + ".txt"
         args = args[0].split()
+        
     elif ' < ' in user_input:
         args = user_input.split(' < ')
         inputFlag = True
-        input_name = args[len(args)-1].strip()
+        args[len(args)-1] = args[len(args)-1].strip()
+
     else:
         args = user_input.split()
 
-    cmd = args[0].strip()      # command to run
-    print(args)
-    
-    rc = os.fork()
+    rc = os.fork()    # begin forking
 
-    if rc < 0:
+    if rc < 0:      #fork failed
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
 
-    elif rc == 0:                   # child
-        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                     (os.getpid(), pid)).encode())
-        if "/bin/" in cmd:
-            cmd = cmd.replace("/bin/", "")
+    elif rc == 0:      # child
+        if "/bin/" in args[0]:
+            args[0] = args[0].replace("/bin/", "")
             
         if outputFlag:
             os.close(1)                 # redirect child's stdout
             sys.stdout = open(output_file, "w")
-            fd = sys.stdout.fileno() # os.open("output_shell.txt", os.O_CREAT)
-            os.set_inheritable(fd, True)
-            os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+            os.set_inheritable(1, True)
 
-        if pipeFlag:
-            os.close(r)
-            w = os.fdopen(w, 'w')
-            fd = w.fileno()
+        if inputFlag:
+            os.close(0)
+            sys.stdin = open(args[len(args)-1], "r")
+            fd = sys.stdin.fileno()
             os.set_inheritable(fd, True)
-            os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+            args = [args[0]]
             
-        run_program(args, cmd)
-        
-        os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
+        if pipeFlag:
+            os.close(1)
+            os.dup(pw)
+            for fd in (pr, pw):
+                os.close(fd)
+            
+        run_execve(args)
         os.write(2, ("Command not found.\n").encode())
-        w.close()
         sys.exit(1)                 # terminate with error 1
 
     else:                           # parent (forked ok)
-        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                     (pid, rc)).encode())
         if not pipeFlag:
             childPidCode = os.wait()
         else:
-            os.close(w)
-            r = os.fdopen(r)
-            str = r.read()
-            run_program([processes[1], r], processes[1].split()[0])
-            
-        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
-                     childPidCode).encode())
-
-def startShell():
-    output_file = "output_shell.txt"       # default output file
-    os.write(1, ("Welcome to the shell.\n").encode())
+            os.close(0)
+            os.dup(pr)
+            for line in fileinput.input():
+                print("From child: <%s?" % line)
+            #run_execve([processes[1], pr], processes[1])
+            for fd in (pw, pr):
+                os.close(fd)
+        
+def startShell(user_input, e):
     while True:
-        user_input = input("$ ")         # request command and args from user
-        if user_input == 'exit':       # exit shell if command is "exit"
+        if user_input == "" or "cd" in user_input:
+            user_input = input(e["PS1"])         # request command and args from user
+            continue
+        if "\n" in user_input or "\\n" in user_input:
+            #os.write(1, ("user input: %s \n" % user_input).encode())
+            for command in user_input.split("\\n"):
+                #os.write(1, ("command: %s \n" % command).encode())
+                if len(command) > 3:
+                    run_command(command.strip())
+            user_input = ""
+            continue
+        if user_input == "exit":       # exit shell if command is "exit"
             os.write(1, ("Thank you for using the shell.\n").encode())
             break
-        if user_input == "":
-            continue
-        args = user_input.split()
-        if args[0] == "cd":
-            if len(args) < 2:
-                continue
-            if args[1] == "..":
-                currentDir = os.getcwd()
-                newDir = currentDir.rsplit('/', 1)[0]
-                os.chdir(newDir)
-                continue
-            else:
-                os.chdir(args[1])
-                continue
-        run_command(user_input, output_file)
-    
-startShell()
+        if "cd" in user_input:
+            args = user_input.split()
+            if args[0].strip() == "cd":
+                if len(args) < 2:
+                    continue
+                if args[1].strip() == "..":
+                    currentDir = os.getcwd()
+                    newDir = currentDir.rsplit('/', 1)[0]
+                    os.chdir(newDir)
+                    continue
+                else:
+                    os.chdir(args[1].strip())
+                    continue
+        run_command(user_input)
+        user_input = ""
+        
+os.write(1, ("Welcome to the shell.\n").encode())
+e = os.environ
+os.environ["PS1"] = ""
+startShell("", e)
